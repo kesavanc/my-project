8b7855f51d039ad3ec331274277bc2f0dfc5994b
kesavan <53558545+kesavanc@users.noreply.github.com>
2020-01-20 13:41:52 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,21 @@
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi kes");
+		
+		int tamil=70;
+		int english=90;
+		int science=95;
+		int total=(tamil+english+science);
+		float avg=(tamil+english+science)/3;
+		System.out.println("tamil="+tamil);
+		System.out.println("english="+english);
+		System.out.println("science="+science);
+		System.out.println("total="+total);
+		System.out.println("avg="+avg);
+
+	}
+
+}
---
+++ firstcode.java
@@ -0,0 +1,14 @@
+package kesavan;
+
+public class firstcode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num=5;
+		int num2=10;
+		
+		System.out.println(num*num2);
+	}
+
+}
